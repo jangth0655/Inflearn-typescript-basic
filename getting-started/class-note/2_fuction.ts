9c27f7d5f92533944b157a544e2d7111fd321008
JANGTAEHEE <jangth0655@gmail.com>
2022-01-31 23:26:39 +0900

    typeing todo project

--- getting-started/class-note/2_fuction.ts
+++ getting-started/class-note/2_fuction.ts
@@ -1,11 +1,13 @@
-//반환값 타입, 옵셔널 타입
-function sum(a: number, b: number, c?: number): number {
-  return a + b + c;
-}
+{
+  //반환값 타입, 옵셔널 타입
+  function sum(a: number, b: number, c?: number): number {
+    return a + b + c;
+  }
 
-sum(1, 2);
+  sum(1, 2);
 
-//2 반환값에 타입을 정의
-function add(): number {
-  return 10;
+  //2 반환값에 타입을 정의
+  function add(): number {
+    return 10;
+  }
 }
